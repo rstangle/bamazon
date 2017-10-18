bca3eaf78fa3c40ada40cb640278b7dd0fcf2f07
Stangle <RStangle@firstam.com>
2017-10-18 14:57:29 -0400

    Some working code and video demo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bamazon
+
+PURPOSE:
+The purpose of Bamazon is to allow online shoppers to see the inventory and make a purchase selection in the Bamazon Online Store.  Users will be shown the current inventory levels of in-stock items. They will then make a selection and once verified, make the purchase.  If the item is out of stock, it will not be shown.  If the user selects an amount that is greater than the current inventory levels, the system will alert the user and not allow the sale to go through. 
+
+Once the sale has been verified, the stock levels will be adjusted accordingly.
+
+DEMO:
+https://drive.google.com/file/d/0ByaawHGX1NDfdG9jZ3A3c00weTg/view?usp=sharing
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 // Require mysql and inquierer to run app.
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+		var choiceArray = [];
+		
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "127.0.0.1",
@@ -19,8 +20,7 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+  start();  // run the start function after the connection is made to prompt the user
 });
 
 // function which prompts the user for what action they should take
@@ -31,19 +31,13 @@ function start() {
       message: "Welcome to 'Daily W.O.D. Gear' by Bamazon. \nHere are our daily items for sale. Hit enter to continue.",
     })
     .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      // if (answer.postOrBid.toUpperCase() === "POST") {
-      //   postAuction();
-      // }
-      // else {
-      //   bidAuction();
-      // }
       afterConnection();
       // selectItem();
     });
 }
 
 function afterConnection() {
+	// LISTS THE ITEMS THAT ARE AVAILABLE IN THE STORE
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++){
@@ -52,51 +46,53 @@ function afterConnection() {
     // console.log(res);
     console.log("-----------------------------------");
     // connection.end();
-    // selectItem();
+    selectItem();
   });
 }
 
+//  THIS IS THE SECTION THAT I CAN'T GET TO WORK.  IT PUSHES TO THE ARRAY, BUT THEN NO IDEA WHAT IT IS DOING
+ // AFTERWARDS OR HOW TO FIX THE ISSUE OF ITEMS BEING DISPLAYED AS 'UNDEFINED'.
 function selectItem() {
+	// SHOWS THE ITEMS IN THE DATABASE WITH QUANTITIES LARGER THAN 0. OTHER WORDS, ONLY ITEMS IN STOCK.
 	connection.query("SELECT * FROM products WHERE quantity > 0;", function(err, res) {
- 	 if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to buy
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "list",
-          // NOT SURE WHY THIS IS HERE.  CONSOLE.LOG THE ARRAY JUST SHOWS THE LOOPED ITEMS. NOT WHAT I THOUGHT IT DID.
-            // IS THIS EVEN NEEDED? I DON'T KNOW...
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < res.length; i++) {
-              choiceArray.push({productName: res[i].product_name.toUpperCase(), quantity: res[i].quantity});
-              // choiceArray.push(res[i].product_name.toUpperCase(), res[i].quantity);
+	 	 if (err) throw err;
 
-              console.log("Choice Array: " + choiceArray[i].productName + " | " + "Quantity: " + choiceArray[i].quantity);
-            }
-            return choiceArray;
-          },
-          message: "What item would you like to purchase?"
-        },
-        // {
-        //   type: "confirm",
-        //   message: "Are you sure:",
-        //   name: "confirm",
-        //   default: true
-        // }
-      ])
+		for (var i = 0; i < res.length; i++) {
+	  		choiceArray.push({productName: res[i].product_name.toUpperCase(), quantity: res[i].quantity});
+	  		console.log("Choice Array: " + choiceArray[i].productName + " | " + "Quantity: " + choiceArray[i].quantity);
+		    // once you have the items, prompt the user for which they'd like to buy
+		 }
+	    inquirer
+	      .prompt([
+	        {
+	        	name: "choice",
+	        	type: "list",
+	        	// NOT SURE WHY THIS IS HERE.  CONSOLE.LOG THE ARRAY JUST SHOWS THE LOOPED ITEMS.
+	              // IS THIS EVEN NEEDED? I DON'T KNOW...
+	        	choices: choiceArray,
+	          	message: "What item would you like to purchase?"
+	        },
+	    	{
+	      		type: "confirm",
+	      		message: "Are you sure:",
+	      		name: "confirm",
+	      		default: true
+	    	}
+	      ])
+	      .then(function(answer) {
+	      	console.log("------------------");
+	      	console.log(answer);  // SHOWS THE SELECTION AS AN 'UNDEFINED' ITEM. NOTHING ELSE TO DO SO IT ENDS THE CONNECTION.
+
 
       //GET TO HERE AND APPLICATION QUITS. HONESTLY, I DON'T KNOW WHAT I'M DOING... JUST COPYING, PASTING
       // AND HOPING FOR THE BEST... SO FAR NOT SO GREAT.  :(
 
       // I KNOW I WANT TO CHECK TO SEE IF THE ITEM SELECTED IS IN STOCK.
+     
       // IF IT IS IN STOCK, REMOVE FROM STOCK AND UPDATE THE SQL DATABASE. THEN SHOW THE CUSTOMER
         // THE TOTAL COST OF THEIR ORDER.
-      // IF NOT IN STOCK, POST A MESSAGE OF "INSUFFICIENT STOCK. PLEASE LOWER QUANTITY".
      
-      .then(function(answer) {
-      	console.log(answer);
+      // IF NOT IN STOCK, POST A MESSAGE OF "INSUFFICIENT STOCK. PLEASE LOWER QUANTITY".
 
       	// get quantity
       	// make sure you have enough in stock
@@ -106,43 +102,7 @@ function selectItem() {
       	// update db quantity
       		// look up the UPDATE syntax for Workbench
 
-
-
-        // get the information of the chosen item
-      //   var chosenItem;
-      //   for (var i = 0; i < res.length; i++) {
-      //     if (res[i].item_id === answer.choice) {
-      //       chosenItem = res[i];
-
-      //     }
-      //   }
-
-      //   // determine if bid was high enough
-      //   if (chosenItem.quantity > 0) {
-      //     // bid was high enough, so update db, let the user know, and start over
-      //     connection.query(
-      //       "UPDATE products SET ? WHERE ?",
-      //       [
-      //         {
-      //           quantity: -1
-      //         },
-      //         {
-      //           id: chosenItem.id
-      //         }
-      //       ],
-      //       function(error) {
-      //         if (error) throw err;
-      //         console.log("Order placed successfully!");
-      //         start();
-      //       }
-      //     );
-      //   }
-      //   else {
-      //     // bid wasn't high enough, so apologize and start over
-      //     console.log("Your Order was too big. Try a lesser quantity");
-      //     start();
-      //   }
-      });
-          connection.end();
-  });
+      		});
+	    });
+     connection.end();
 }
